d5c8c5d2fa143f7d50b7cb90757f09e4939753b7
Deniz <77787415+deniz244@users.noreply.github.com>
2023-08-04 12:15:56 +0300

    reducers.js reducer function , logger paketi ve index.js içinde redux için provider, store, thunk eklendi.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 
+import { legacy_createStore as createStore, applyMiddleware } from "redux";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import { Provider } from "react-redux";
 
+import { reducer } from "./reducers";
+
+import thunk from "redux-thunk";
+import logger from "redux-logger";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const store = createStore(reducer, applyMiddleware(thunk, logger));
 
 root.render(
-  <BrowserRouter>
-    <>
-      <App />
-    </>
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <>
+        <App />
+      </>
+    </BrowserRouter>
+  </Provider>
 );
